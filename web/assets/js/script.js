8eedcc9d6edd06ae6cf7d13dba7d287da07bd50f
Shankar <shankarbus04082001@hotmail.com>
2025-05-03 21:03:55 +0530

    Use kaadu-ui

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -2,11 +2,16 @@ import { setupEcgSlider } from './Slider.js';
 import { defineLimbElectrodes } from './Measurement.js';
 import { drawPhaseVectorInHeart, setupHeartCanvas } from './Drawing.js';
 import { CardiacElectricalCycle } from './CardiacCycle.js';
+import { enableStickyHeader, enableHamburgerMenu, setupMessagePopup, showMessagePopup } from 'https://shankarbus.github.io/kaadu-ui/kaadu-ui.js';
 
 let currentCardiacCycle = null;
 
 // Function to initialize the application
 async function initApp() {
+    enableStickyHeader();
+    enableHamburgerMenu();
+    setupMessagePopup();
+    // showMessagePopup('Welcome to the ECG Simulation! This simulation will help you understand the electrical activity of the heart. Use the controls to navigate through the cardiac cycle phases.');
     currentCardiacCycle = await CardiacElectricalCycle.getNormalCycle();
     defineLimbElectrodes();
     await setupEcgSlider(currentCardiacCycle);
